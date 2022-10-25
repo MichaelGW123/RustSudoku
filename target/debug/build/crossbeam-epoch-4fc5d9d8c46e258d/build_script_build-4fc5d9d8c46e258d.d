c7b693d07b478e5e574754b96db1749c0739b556
Michael Williamson <MichaelGW123@gmail.com>
2022-10-24 20:11:37 -0700

    changed directory

--- target/debug/build/crossbeam-epoch-4fc5d9d8c46e258d/build_script_build-4fc5d9d8c46e258d.d
+++ target/debug/build/crossbeam-epoch-4fc5d9d8c46e258d/build_script_build-4fc5d9d8c46e258d.d
@@ -1,6 +1,6 @@
-/home/michael/Documents/Practice/Rust Code/hello_world/target/debug/build/crossbeam-epoch-4fc5d9d8c46e258d/build_script_build-4fc5d9d8c46e258d: /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-epoch-0.9.9/build.rs /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-epoch-0.9.9/no_atomic.rs
+/home/michael/Documents/GitHub/RustSudoku/target/debug/build/crossbeam-epoch-4fc5d9d8c46e258d/build_script_build-4fc5d9d8c46e258d: /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-epoch-0.9.9/build.rs /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-epoch-0.9.9/no_atomic.rs
 
-/home/michael/Documents/Practice/Rust Code/hello_world/target/debug/build/crossbeam-epoch-4fc5d9d8c46e258d/build_script_build-4fc5d9d8c46e258d.d: /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-epoch-0.9.9/build.rs /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-epoch-0.9.9/no_atomic.rs
+/home/michael/Documents/GitHub/RustSudoku/target/debug/build/crossbeam-epoch-4fc5d9d8c46e258d/build_script_build-4fc5d9d8c46e258d.d: /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-epoch-0.9.9/build.rs /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-epoch-0.9.9/no_atomic.rs
 
 /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-epoch-0.9.9/build.rs:
 /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/crossbeam-epoch-0.9.9/no_atomic.rs:
--- target/debug/build/rayon-d9fae4ff6fe0a473/build_script_build-d9fae4ff6fe0a473.d
+++ target/debug/build/rayon-d9fae4ff6fe0a473/build_script_build-d9fae4ff6fe0a473.d
@@ -1,5 +1,5 @@
-/home/michael/Documents/Practice/Rust Code/hello_world/target/debug/build/rayon-d9fae4ff6fe0a473/build_script_build-d9fae4ff6fe0a473: /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/rayon-1.5.3/build.rs
+/home/michael/Documents/GitHub/RustSudoku/target/debug/build/rayon-d9fae4ff6fe0a473/build_script_build-d9fae4ff6fe0a473: /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/rayon-1.5.3/build.rs
 
-/home/michael/Documents/Practice/Rust Code/hello_world/target/debug/build/rayon-d9fae4ff6fe0a473/build_script_build-d9fae4ff6fe0a473.d: /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/rayon-1.5.3/build.rs
+/home/michael/Documents/GitHub/RustSudoku/target/debug/build/rayon-d9fae4ff6fe0a473/build_script_build-d9fae4ff6fe0a473.d: /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/rayon-1.5.3/build.rs
 
 /home/michael/.cargo/registry/src/github.com-1ecc6299db9ec823/rayon-1.5.3/build.rs:
